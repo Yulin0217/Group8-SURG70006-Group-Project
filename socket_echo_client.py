6c1858f44eff46777c85f5ededd869beefcc2c38
JerryJieLi666666 <167567879+JerryJieLi666666@users.noreply.github.com>
2024-11-08 17:40:37 +0000

    Update socket_echo_client.py

--- socket_echo_client.py
+++ socket_echo_client.py
@@ -1,42 +1,26 @@
 import socket
 import sys
-import time
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
+# Connect the socket to the server's address and port
 server_address = ('172.26.99.133', 10000)
-print('starting up on %s port %s' % server_address, file=sys.stderr)
-sock.bind(server_address)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+try:
+    while True:
+        # Receive data from server
+        data = sock.recv(32)
+        if data:
+            # Split received data by commas and convert to integers
+            displacement = [int(value) for value in data.decode('utf-8').split(',')]
+            print >>sys.stderr, 'received displacement:', displacement
+        else:
+            print >>sys.stderr, 'no more data from server'
+            break
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection', file=sys.stderr)
-    connection, client_address = sock.accept()
-
-    try:
-        print('connection from', client_address, file=sys.stderr)
-
-        # Send (x, y, z) displacement data in a loop
-        x, y, z = 0, 0, 0
-        while True:
-            # Format displacement data as a comma-separated string
-            message = f"{x},{y},{z}"
-            print(f'sending displacement: {message}', file=sys.stderr)
-            connection.sendall(message.encode())
-
-            # Update (x, y, z) values (increment as a demonstration)
-            x += 1
-            y += 2
-            z += 3
-
-            # Delay for readability (optional)
-            time.sleep(1)
-
-    finally:
-        # Clean up the connection
-        connection.close()
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
