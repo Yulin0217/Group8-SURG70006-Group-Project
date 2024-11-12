f63140b2e7cdb2a5ebae52c2ee827bafed846059
JerryJieLi666666 <167567879+JerryJieLi666666@users.noreply.github.com>
2024-11-12 14:40:36 +0000

    Update socket_echo_client.py

--- socket_echo_client.py
+++ socket_echo_client.py
@@ -5,17 +5,17 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the server's address and port
-server_address = ('172.26.99.133', 10000)
+server_address = ('172.26.211.137', 10000)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:
     while True:
         # Receive data from server
-        data = sock.recv(32)
+        data = sock.recv(1024)
         if data:
             # Split received data by commas and convert to integers
-            displacement = [int(value) for value in data.decode('utf-8').split(',')]
+            displacement = [float(value) for value in data.decode('utf-8').split(',')]
             print >>sys.stderr, 'received displacement:', displacement
         else:
             print >>sys.stderr, 'no more data from server'
